7603685d61951f51ec61381d5817fc7725b6bedc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-05 16:08:52 +0300

    Added a gradient background to the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-	return <div className='h-full'></div>
+	return (
+		<main className='h-full flex flex-col items-center justify-center bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500'></main>
+	)
 }
