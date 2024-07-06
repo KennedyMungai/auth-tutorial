445ff348e0bc4074e6e4f4706c9a9a42bc0c0436
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-06 12:17:44 +0300

    Added the props to CardWrapper component

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,9 +1,18 @@
-type Props = {
+import CardWrapper from '@/components/auth/card-wrapper'
 
-}
+type Props = {}
 
 const LoginForm = () => {
-return <div>LoginForm</div>
+	return (
+		<CardWrapper
+			headerLabel='Welcome Back'
+			backButtonLabel="Don't have an account?"
+			backButtonHref='/auth/register'
+			showSocial
+		>
+			LoginForm
+		</CardWrapper>
+	)
 }
 
 export default LoginForm
