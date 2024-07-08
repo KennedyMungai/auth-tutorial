81dbe775bb2e6776cfee31b15f8cc1bb22d9062d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-08 14:26:25 +0300

    Added the message object to the email inside the loginSchema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod'
 
 export const loginSchema = z.object({
-	email: z.string().email(),
+	email: z.string().email({ message: 'Email is required' }),
 	password: z.string().min(6)
 })
