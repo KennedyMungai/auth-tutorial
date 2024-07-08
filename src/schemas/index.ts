cca3249313e096a3c32f194fc5aff9bfe9dc69f1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-08 14:32:54 +0300

    Made some changes to the passwords schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,7 +2,5 @@ import * as z from 'zod'
 
 export const loginSchema = z.object({
 	email: z.string().email({ message: 'Email is required' }),
-	password: z
-		.string({ message: 'Password is required' })
-		.min(6, { message: 'Password is too short' })
+	password: z.string().min(1, { message: 'Password is required' })
 })
