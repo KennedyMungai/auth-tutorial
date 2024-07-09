8fdd9197038aa06dfe818e156cd91359abac835d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-09 09:10:37 +0300

    Created the loaing page for the app

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+const LoadingPage = () => {
+	return (
+		<div className='bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500 flex items-center justify-center h-full'>
+			<Image src='./logo.png' width={60} height={60} alt='Logo' />
+		</div>
+	)
+}
+
+export default LoadingPage
