e7b0e5322bc0f6e436a87f767ca9a05ebbabbdb6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-09 10:39:36 +0300

    Added some config for the auth file

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { db } from '@/lib/db'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+	adapter: PrismaAdapter(db),
+	session: { strategy: 'jwt' },
 	...authConfig
 })
