4b822cc23f18f60e6db88fb5bedd761c6f95377b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-09 10:37:04 +0300

    Implemented the auth and auth-config logic

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,7 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 import NextAuth from 'next-auth'
-import github from 'next-auth/providers/github'
-import google from 'next-auth/providers/google'
-
-const prisma = new PrismaClient()
+import authConfig from './auth.config'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-	adapter: PrismaAdapter(prisma),
-	providers: [github, google]
+	...authConfig
 })
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from '@/auth'
+import { auth } from '@/auth'
+
+export default auth((req) => {
+	const isLoggedIn = !!req.auth
+
+	console.log('ROUTE: ', req.nextUrl.pathname)
+	console.log('Is Logged In: ', isLoggedIn)
+})
+
+export const config = {
+	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+}
