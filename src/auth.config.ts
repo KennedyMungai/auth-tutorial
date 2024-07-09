9bfa62dfe18c6554ab54ab117bce2d9c1eb8101c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-09 11:40:20 +0300

    Implemented the credentials provider inside the auth config file

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,32 @@
+import { getUserByEmail } from '@/data/user'
+import { loginSchema } from '@/schemas/index'
+import bcrypt from 'bcryptjs'
 import { NextAuthConfig } from 'next-auth'
-import github from 'next-auth/providers/github'
-import google from 'next-auth/providers/google'
+import credentials from 'next-auth/providers/credentials'
 
 export default {
-	providers: [github, google]
+	providers: [
+		credentials({
+			async authorize(credentials) {
+				const validatedFields = loginSchema.safeParse(credentials)
+
+				if (validatedFields.success) {
+					const { email, password } = validatedFields.data
+
+					const user = await getUserByEmail(email)
+
+					if (!user || !user.password) return null
+
+					const passwordsMatch = await bcrypt.compare(
+						password,
+						user.password
+					)
+
+					if (passwordsMatch) return user
+				}
+
+				return null
+			}
+		})
+	]
 } satisfies NextAuthConfig
