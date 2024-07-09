2e20d8a9ccadba67e9043a519cd0060be60f3bcf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-09 09:12:03 +0300

    Updated the src of the image

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const LoadingPage = () => {
 	return (
 		<div className='bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500 flex items-center justify-center h-full'>
-			<Image src='./logo.png' width={60} height={60} alt='Logo' />
+			<Image src='logo.png' width={60} height={60} alt='Logo' />
 		</div>
 	)
 }
