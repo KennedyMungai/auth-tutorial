d205ceb60229e512302491360d3839909aeba7ed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-10 09:58:00 +0300

    Added a classname to make the image oulse when loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 const LoadingPage = () => {
 	return (
 		<div className='bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500 flex items-center justify-center h-full'>
-			<Image src='/logo.png' width={60} height={60} alt='Logo' />
+			<Image
+				src='/logo.png'
+				width={60}
+				height={60}
+				alt='Logo'
+				className='animate-pulse'
+			/>
 		</div>
 	)
 }
