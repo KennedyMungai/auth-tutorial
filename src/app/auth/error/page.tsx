607175f3305225ed165ca8f48c7e67ec98b52969
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-10 10:50:53 +0300

    Fixed a dumb mistake

---
+++ src/app/auth/error/page.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+	return (
+		<div className='h-full flex items-center justify-center bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500'>
+			ErrorPage
+		</div>
+	)
+}
+
+export default ErrorPage
