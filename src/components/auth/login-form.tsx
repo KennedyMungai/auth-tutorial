0f23367d997182788d2a94e641bafa9799675661
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-10 14:21:37 +0300

    Uncommented the setSuccess hook function

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -48,8 +48,7 @@ const LoginForm = () => {
 		startTransition(() => {
 			login(values).then((data) => {
 				setError(data?.error)
-				// TODO: For when we have 2FA
-				// setSuccess(data?.success)
+				setSuccess(data?.success)
 			})
 		})
 	}
