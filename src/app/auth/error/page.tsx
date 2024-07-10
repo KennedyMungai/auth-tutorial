3398bab16bc83b72d3234867bec738e4ecf3be05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-10 10:53:51 +0300

    Created and rendered the error card component

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,7 +1,9 @@
+import ErrorCard from '@/components/auth/error-card'
+
 const ErrorPage = () => {
 	return (
 		<div className='h-full flex items-center justify-center bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500'>
-			ErrorPage
+			<ErrorCard />
 		</div>
 	)
 }
---
+++ src/components/auth/error-card.tsx
@@ -0,0 +1,9 @@
+import { Card } from '@/components/ui/card'
+
+type Props = {}
+
+const ErrorCard = () => {
+	return <Card>ErrorCard</Card>
+}
+
+export default ErrorCard
