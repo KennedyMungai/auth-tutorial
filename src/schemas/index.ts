da849315d451de0603c79b2f53f3322a48068195
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 16:14:17 +0300

    Added the code field to the login schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,7 +2,8 @@ import * as z from 'zod'
 
 export const loginSchema = z.object({
 	email: z.string().email({ message: 'Email is required' }),
-	password: z.string().min(1, { message: 'Password is required' })
+	password: z.string().min(1, { message: 'Password is required' }),
+	code: z.string().min(1, { message: 'Code is required' })
 })
 
 export const resetSchema = z.object({
