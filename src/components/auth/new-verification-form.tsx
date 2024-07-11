b65f548dc432b24fa203bd5735ad77d05b0c5a19
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 10:27:37 +0300

    Added some markup and logic to the new verification form

--- src/components/auth/new-verification-form.tsx
+++ src/components/auth/new-verification-form.tsx
@@ -1,19 +1,33 @@
 'use client'
 
+import { newVerification } from '@/actions/new-verification'
 import CardWrapper from '@/components/auth/card-wrapper'
+import FormError from '@/components/form-error'
+import FormSuccess from '@/components/form-success'
 import { Loader2 } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
-import { useCallback, useEffect } from 'react'
-
-type Props = {}
+import { useCallback, useEffect, useState } from 'react'
 
 const NewVerificationForm = () => {
 	const searchParams = useSearchParams()
 
-	const token = searchParams.get('token')
+	const [error, setError] = useState<string | undefined>('')
+	const [success, setSuccess] = useState<string | undefined>('')
+
+	const token = searchParams.get('token') as string
 
 	const onSubmit = useCallback(() => {
-		console.log(token)
+		if (!token) {
+			setError('Missing Token')
+			return
+		}
+
+		newVerification(token)
+			.then((data) => {
+				setSuccess(data.success)
+				setError(data.error)
+			})
+			.catch(() => setError('Something went wrong'))
 	}, [token])
 
 	useEffect(() => onSubmit(), [onSubmit])
@@ -24,8 +38,10 @@ const NewVerificationForm = () => {
 			backButtonLabel={'Back to Login'}
 			backButtonHref={'/auth/login'}
 		>
-			<div className='flex items-center w-full justify-center'>
+			<div className='flex flex-col gap-y-2 items-center w-full justify-center'>
 				<Loader2 className='size-8 animate-spin text-neutral-400' />
+				<FormSuccess message={success} />
+				<FormError message={error} />
 			</div>
 		</CardWrapper>
 	)
