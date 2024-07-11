6e1b417e857907d71804bdff7d8fe55db11b1cd2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 11:39:27 +0300

    Wrote the logic to generate the password reset token and sent the password reset email to a users email address

--- src/actions/reset.ts
+++ src/actions/reset.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import { getUserByEmail } from '@/data/user'
+import { sendPasswordResetEmail } from '@/lib/mail'
+import { generatePasswordResetToken } from '@/lib/tokens'
 import { resetSchema } from '@/schemas'
 import * as z from 'zod'
 
@@ -15,7 +17,12 @@ export const reset = async (values: z.infer<typeof resetSchema>) => {
 
 	if (!existingUser) return { error: 'Email not found!' }
 
-	// TODO: Generate token and send email
+	const passwordResetToken = await generatePasswordResetToken(email)
+
+	await sendPasswordResetEmail(
+		passwordResetToken.email,
+		passwordResetToken.token
+	)
 
 	return { success: 'Reset Email Sent' }
 }
