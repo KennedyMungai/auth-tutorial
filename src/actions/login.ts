cf95be28949ef62741b3a2f92dea2de884141c45
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 09:33:42 +0300

    Sending the verification mail when logging in

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -2,6 +2,7 @@
 
 import { signIn } from '@/auth'
 import { getUserByEmail } from '@/data/user'
+import { sendVerificationEmail } from '@/lib/mail'
 import { generateVerificationToken } from '@/lib/tokens'
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 import { loginSchema } from '@/schemas'
@@ -23,6 +24,11 @@ export const login = async (values: z.infer<typeof loginSchema>) => {
 	if (!existingUser.emailVerified) {
 		const verificationToken = await generateVerificationToken(
 			existingUser.email
+		)
+
+		await sendVerificationEmail(
+			verificationToken.email,
+			verificationToken.token
 		)
 
 		return { success: 'Confirmation Email Sent!' }
