92543a426a3e844bce80213399c2d7ffe993969e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 09:33:08 +0300

    Added a console log inside the signIn callback

--- src/auth.ts
+++ src/auth.ts
@@ -20,6 +20,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 	},
 	callbacks: {
 		async signIn({ user, account }) {
+			console.log({ user, account })
+
 			// Allow OAuth without email verification
 			if (account?.provider !== 'credentials') return true
 
