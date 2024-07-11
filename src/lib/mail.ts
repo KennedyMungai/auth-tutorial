7ecfdbc4fbdb1b3cf05f2555ab2ba56bf6d883b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-11 09:21:27 +0300

    Updated the sender email address

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 	const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
 
 	await resend.emails.send({
-		from: 'auth-corp@corporate.com',
+		from: 'onboarding@resend.dev',
 		to: email,
 		subject: 'Please confirm your email',
 		html: `<p>Please confirm your email by clicking <a href="${confirmLink}">here</a>:</p>`
