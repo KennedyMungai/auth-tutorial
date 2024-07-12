588153ab3fefd2a4ba0c921b77c171aa8b34c2b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-12 11:14:56 +0300

    Rendered the navbar on the layout of the protected routes

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import NavBar from './_components/navbar'
 
 type Props = {
 	children: ReactNode
@@ -7,6 +8,7 @@ type Props = {
 const ProtectedLayout = ({ children }: Props) => {
 	return (
 		<div className='h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500'>
+			<NavBar />
 			{children}
 		</div>
 	)
