c02ba7db613ac9957708607becf41da6f282d289
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-12 12:52:38 +0300

    Added some markup to the component

--- src/app/(protected)/server/page.tsx
+++ src/app/(protected)/server/page.tsx
@@ -1,9 +1,10 @@
+import UserInfo from '@/components/user-info'
 import { currentUser } from '@/lib/auth'
 
 const ServerPage = async () => {
 	const user = await currentUser()
 
-	return <div>{JSON.stringify(user, null, 2)}</div>
+	return <UserInfo label={'🖥️ Server Component'} user={user} />
 }
 
 export default ServerPage
