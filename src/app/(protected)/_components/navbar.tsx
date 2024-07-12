ee90def939435e7045923b5b83d610fc2b75fde8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-12 11:14:17 +0300

    Created the navbar component

---
+++ src/app/(protected)/_components/navbar.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const NavBar = () => {
+	const pathname = usePathname()
+
+	return (
+		<nav className='bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm'>
+			<div className='flex gap-x-2'>
+				<Link href={'/server'}>
+					<Button
+						variant={pathname === '/server' ? 'default' : 'outline'}
+					>
+						Server
+					</Button>
+				</Link>
+				<Link href={'/client'}>
+					<Button
+						variant={pathname === '/client' ? 'default' : 'outline'}
+					>
+						Client
+					</Button>
+				</Link>
+				<Link href={'/admin'}>
+					<Button
+						variant={pathname === '/admin' ? 'default' : 'outline'}
+					>
+						Admin
+					</Button>
+				</Link>
+				<Link href={'/settings'}>
+					<Button
+						variant={
+							pathname === '/settings' ? 'default' : 'outline'
+						}
+					>
+						Settings
+					</Button>
+				</Link>
+			</div>
+			<p>User Button</p>
+		</nav>
+	)
+}
+
+export default NavBar
