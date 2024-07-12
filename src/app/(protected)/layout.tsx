333573579f8669af453a7147cc583f200607b618
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-12 11:01:41 +0300

    Made some updates to the background gradient

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ProtectedLayout = ({ children }: Props) => {
 	return (
-		<div className='h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gradient-to-br from-indigo-500 via-violet-500 to-purple-500'>
+		<div className='h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gradient-to-br from-violet-500 via-indigo-500 to-purple-500'>
 			{children}
 		</div>
 	)
